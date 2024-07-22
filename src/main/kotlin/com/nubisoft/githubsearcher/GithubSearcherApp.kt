9b1599066397f760644e63dc81f45b90d4b98165
Kamil Kotlarz <kamil.kotlarz@tui.co.uk>
2024-07-22 14:42:19 +0200

    KAM-03: main function taken to class

--- src/main/kotlin/com/nubisoft/githubsearcher/GithubSearcherApp.kt
+++ src/main/kotlin/com/nubisoft/githubsearcher/GithubSearcherApp.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class GithubSearcherApp
-
-fun main(args: Array<String>) {
-    runApplication<GithubSearcherApp>(*args)
+class GithubSearcherApp {
+    fun main(args: Array<String>) {
+        runApplication<GithubSearcherApp>(*args)
+    }
 }
